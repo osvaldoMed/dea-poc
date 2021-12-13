c9503d1ad544b72ef6e7a032601c0d17a5c04e2f
Osvaldo Rosales <osvaldo.rosales@medtrainer.com>
2021-12-13 15:46:01 -0800

    LSFB-37286 - [UPDATE] scrapy-only testing and settings update.

--- scrapy-dea/scrapy-dea/spiders/deaSpider.py
+++ scrapy-dea/scrapy-dea/spiders/deaSpider.py
@@ -15,13 +15,15 @@ class DEASpider(scrapy.Spider):
                                     callback=self.login_1,
                                     dont_filter=True)
         request.meta['cookiejar'] = i
-        self.logger.info(f'INITIAL REQUEST at {request.url}')
+        self.logger.info(f'INITIAL REQUEST headers: {request.headers}')
     
         return [request]
 
 
     def login_1(self, response):
-        open_in_browser(response)
+        #open_in_browser(response)
+        self.logger.info(f'RECEIVED start_request response')
+        self.logger.info(f'HEADERS in start_request response: {response.headers}')
 
         formdata = {'pform:deaNumber':'FE9093028'}
         formRequest = FormRequest.from_response(response=response,
@@ -29,15 +31,17 @@ class DEASpider(scrapy.Spider):
                                         callback=self.login_2,
                                         meta={'cookiejar':response.meta['cookiejar']},
                                         dont_filter=True)
-        self.logger.info(f'FIRST LOGIN REQUEST with {formdata} \n to {formRequest.url}')
-        self.logger.info(f'FIRST LOGIN REQUEST COOKIES with {formRequest.headers}')
+
+        self.logger.info(f'SENDING login_1 REQUEST with \n formdata:{formdata} \n to {formRequest.url} \n and headers:{formRequest.headers}')
 
         yield formRequest
 
 
 
     def login_2(self, response):
-        open_in_browser(response)
+        #open_in_browser(response)
+        self.logger.info(f'RECEIVED login_1 response')
+        self.logger.info(f'HEADERS in login response: {response.headers}')
 
         formdata = {
             'csa_lastName': 'Estler', 
@@ -53,13 +57,14 @@ class DEASpider(scrapy.Spider):
                                         callback=self.login_3,
                                         meta={'cookiejar':response.meta['cookiejar']},
                                         dont_filter=True)
-        self.logger.info(f'SECOND LOGIN REQUEST with {formdata} \n to {formRequest.url}')
+        self.logger.info(f'SENDING login_2 REQUEST with \n formdata:{formdata} \n to {formRequest.url} \n and headers:{formRequest.headers}')
+
         yield formRequest
         
     def login_3(self, response):
 
         self.logger.info(f'RECEIVED LAST LOGIN REQUEST FROM {response.url}')
-        open_in_browser(response)
+        #open_in_browser(response)
 
 
 
