6ff9d685495fdb65b451c96ca94ae7b1cd441c47
Osvaldo Rosales <osvaldo.rosales@medtrainer.com>
2021-12-13 18:31:20 -0800

    LSFB-37286 - [Modified] splash+scrapy and scrapy implementation to monitor the headers of each request. It didnt worked out

--- scrapy-dea/scrapy-dea/spiders/deaSpider.py
+++ scrapy-dea/scrapy-dea/spiders/deaSpider.py
@@ -6,16 +6,20 @@ from scrapy.utils.response import open_in_browser
 
 class DEASpider(scrapy.Spider):
     name = 'dea'
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
+
 
 
     def start_requests(self):
-        i = 1
+        url = 'https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin'
 
         request = scrapy.Request(url='https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin',
                                     callback=self.login_1,
                                     dont_filter=True)
-        request.meta['cookiejar'] = i
-        self.logger.info(f'INITIAL REQUEST headers: {request.headers}')
+        request.meta['cookiejar'] = 1
+
+        self.logger.info(f'INITIAL REQUEST to {request.url}')
+        self.logger.info(f'INITIAL REQUEST HEADERS:  {request.headers}')
     
         return [request]
 
@@ -24,8 +28,12 @@ class DEASpider(scrapy.Spider):
         #open_in_browser(response)
         self.logger.info(f'RECEIVED start_request response')
         self.logger.info(f'HEADERS in start_request response: {response.headers}')
-
-        formdata = {'pform:deaNumber':'FE9093028'}
+        formdata = {
+            'pform': 'pform',
+            'pform:deaNumber':'FE9093028',
+            'pform:validateDeaNumber': '',
+            'javax.faces.ViewState': 'e2s1',
+        }
         formRequest = FormRequest.from_response(response=response,
                                         formdata=formdata,
                                         callback=self.login_2,
@@ -39,7 +47,7 @@ class DEASpider(scrapy.Spider):
 
 
     def login_2(self, response):
-        #open_in_browser(response)
+        open_in_browser(response)
         self.logger.info(f'RECEIVED login_1 response')
         self.logger.info(f'HEADERS in login response: {response.headers}')
 
@@ -62,7 +70,7 @@ class DEASpider(scrapy.Spider):
         yield formRequest
         
     def login_3(self, response):
-
+        open_in_browser(response)
         self.logger.info(f'RECEIVED LAST LOGIN REQUEST FROM {response.url}')
         #open_in_browser(response)
 
--- splash_dea/splash_dea/spiders/deaSpider.py
+++ splash_dea/splash_dea/spiders/deaSpider.py
@@ -8,39 +8,66 @@ from scrapy.utils.response import open_in_browser
 
 class DEASpider(scrapy.Spider):
     name = 'dea'
-    user_agent = 'Mozilla/5.0'
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
 
     def start_requests(self):
         script = """
             function main(splash, args)
             splash:go(args.url)
 
+            local entries = splash:history()
+            local last_response = entries[#entries].response
+
             return {
                 html = splash:html(),
                 png = splash:png(),
                 cookies = splash:get_cookies(),
+                headers = last_response.headers,
                 }
             end"""
 
-        url = 'https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin?execution=e1s1'
+        url = 'https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin'
+        headers_0 = {
+            'Host': 'apps.deadiversion.usdoj.gov',
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="96", "Google Chrome";v="96"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': "Linux",
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+        }
+
         request = SplashRequest(
             url=url,
             callback=self.login_1,
             endpoint='execute',
             session_id=1,
-            args={'lua_source': script},
+            args={'lua_source': script, 'wait':20.},
         )
-        self.logger.info(f'INITIAL REQUEST at {request.url}')
+        self.logger.info(f'INITIAL REQUEST to {request.url}')
+        self.logger.info(f'INITIAL REQUEST HEADERS:  {request.headers}')
         self.logger.info(f'INITIAL REQUEST splash args: {request.meta["splash"]}')
-
     
         return [request]
 
-
     def login_1(self, response):
+        cookies = response.data['cookies']
+        headers = response.data['headers']
+        html = response.data['html']
         self.logger.info(f'RECEIVED start_request response')
-        self.logger.info(f'COOKIES in start_request response: {response.data["cookies"]}')
-        self.logger.info(f'HEADERS in start_request response: {response.headers}')
+        self.logger.info(f'COOKIES in start_request response: {cookies}')
+        self.logger.info(f'HEADERS in start_request response: {headers}')
+        self.logger.info(f'HTML in start_request response: {html}')
+
 
         self.logger.info(f'GENERATING start_request response png.')
         imgdata = base64.b64decode(response.data['png'])
@@ -49,40 +76,98 @@ class DEASpider(scrapy.Spider):
         with open(filename, 'wb') as f:
             f.write(imgdata)
 
-        self.logger.info(f'SAVED start_request response png to {filename}')
-
+    #     self.logger.info(f'SAVED start_request response png to {filename}')
 
         script = """
             function main(splash, args)
             splash:init_cookies(splash.args.cookies)
             splash:go(splash.args.url)
 
+            local entries = splash:history()
+            local last_response = entries[#entries].response
+
             return {
                 html = splash:html(),
                 png = splash:png(),
                 cookies = splash:get_cookies(),
+                headers = last_response.headers,
                 }
             end"""
 
-        formdata = {'pform:deaNumber':'FE9093028'}
-        formRequest = SplashFormRequest(
-            url=response.url,
-            formdata=formdata,
+        url = 'https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin?execution=e2s1'
+        formdata = {
+            'pform': 'pform',
+            'pform:deaNumber':'FE9093028',
+            'pform:validateDeaNumber': '',
+            'javax.faces.ViewState': 'e2s1',
+        }
+
+        request = SplashFormRequest(
+            url=url,
             callback=self.login_2,
+            formdata=formdata,
             endpoint='execute',
             session_id=1,
-            args={'lua_source': script, 'cookies': response.data['cookies'], 'wait': 10.},
-            dont_filter=True,
+            args={'lua_source': script, 'cookies': cookies, 'wait':20.},
         )
-        self.logger.info(f'SENDING login_1 REQUEST with \n formdata:{formdata} \n to {formRequest.url} \n and args:{formRequest.meta["splash"]["args"]}')
 
-        yield formRequest
+
+        self.logger.info(f'login_1 REQUEST to {request.url}')
+        self.logger.info(f'login_1 REQUEST HEADERS:  {request.headers}')
+        self.logger.info(f'login_1 REQUEST splash args: {request.meta["splash"]}')
+
+        return request
+
+    #     self.logger.info(f'GENERATING start_request response png.')
+    #     imgdata = base64.b64decode(response.data['png'])
+    #     filename = './img/dea_01.png'
+
+    #     with open(filename, 'wb') as f:
+    #         f.write(imgdata)
+
+    #     self.logger.info(f'SAVED start_request response png to {filename}')
+
+
+    #     script = """
+    #         function main(splash, args)
+    #         splash:init_cookies(splash.args.cookies)
+    #         splash:go(splash.args.url)
+
+    #         return {
+    #             html = splash:html(),
+    #             png = splash:png(),
+    #             cookies = splash:get_cookies(),
+    #             }
+    #         end"""
+
+    #     formdata = {'pform:deaNumber':'FE9093028'}
+    #     formRequest = SplashFormRequest(
+    #         url=response.url,
+    #         formdata=formdata,
+    #         callback=self.login_2,
+    #         endpoint='execute',
+    #         session_id=1,
+    #         args={'lua_source': script, 'cookies': response.data['cookies'], 'wait': 10.},
+    #         dont_filter=True,
+    #     )
+    # #
+
+    #     self.logger.info(f'SENDING login_1 REQUEST with \n formdata:{formdata} \n to {formRequest.url} \n and args:{formRequest.meta["splash"]["args"]}')
+
+    #     yield formRequest
 
 
 
     def login_2(self, response):
+        cookies = response.data['cookies']
+        headers = response.data['headers']
+        html = response.data['html']
         self.logger.info(f'RECEIVED login_1 response')
-        self.logger.info(f'COOKIES in login_1 response: {response.data["cookies"]}')
+        self.logger.info(f'COOKIES in login_1 response: {cookies}')
+        self.logger.info(f'HEADERS in login_1 response: {headers}')
+        self.logger.info(f'HTML in login_1 response: {html}')
+    #     self.logger.info(f'RECEIVED login_1 response')
+    #     self.logger.info(f'COOKIES in login_1 response: {response.data["cookies"]}')
 
         self.logger.info(f'GENERATING login_1 response png.')
         imgdata = base64.b64decode(response.data['png'])
