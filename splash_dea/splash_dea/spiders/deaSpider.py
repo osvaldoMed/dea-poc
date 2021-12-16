5b136f62db9a60c25bc1a46e8b0e25a4b3f4fa95
Osvaldo Rosales <osvaldo.rosales@medtrainer.com>
2021-12-15 21:37:40 -0800

    LSFB-37286 - [Update] in dea spider, able to login with date of birth

--- splash_dea/splash_dea/spiders/deaSpider.py
+++ splash_dea/splash_dea/spiders/deaSpider.py
@@ -91,6 +91,10 @@ class DEASpider(scrapy.Spider):
                 assert(splash:wait(1))
                 pngTable['5'] = splash:png()
 
+                splash:select('button[type=submit]'):mouse_click()
+                assert(splash:wait(5))
+                pngTable['6'] = splash:png()
+
 
                 
 
@@ -113,7 +117,7 @@ class DEASpider(scrapy.Spider):
             callback=self.login_1,
             endpoint='execute',
             session_id=1,
-            args={'lua_source': script, 'deaNumber': 'FE9093028'},
+            args={'lua_source': script, 'timeout': 3600},
         )
         self.logger.info(f'INITIAL REQUEST to {request.url}')
         self.logger.info(f'INITIAL REQUEST HEADERS:  {request.headers}')
@@ -140,126 +144,4 @@ class DEASpider(scrapy.Spider):
             with open(filename, 'wb') as f:
                 f.write(imgdata)
 
-
-
-    #     self.logger.info(f'SAVED start_request response png to {filename}')
-
-        # script = """
-        #     function main(splash, args)
-        #     splash:init_cookies(splash.args.cookies)
-        #     splash:go(splash.args.url)
-
-        #     local entries = splash:history()
-        #     local last_response = entries[#entries].response
-
-        #     return {
-        #         html = splash:html(),
-        #         png = splash:png(),
-        #         cookies = splash:get_cookies(),
-        #         headers = last_response.headers,
-        #         }
-        #     end"""
-
-        # url = 'https://apps.deadiversion.usdoj.gov/webforms2/spring/validationLogin?execution=e2s1'
-        # formdata = {
-        #     'pform': 'pform',
-        #     'pform:deaNumber':'FE9093028',
-        #     'pform:validateDeaNumber': '',
-        #     'javax.faces.ViewState': 'e2s1',
-        # }
-
-        # request = SplashFormRequest(
-        #     url=url,
-        #     callback=self.login_2,
-        #     formdata=formdata,
-        #     endpoint='execute',
-        #     session_id=1,
-        #     args={'lua_source': script, 'cookies': cookies, 'wait':20.},
-        # )
-
-
-        # self.logger.info(f'login_1 REQUEST to {request.url}')
-        # self.logger.info(f'login_1 REQUEST HEADERS:  {request.headers}')
-        # self.logger.info(f'login_1 REQUEST splash args: {request.meta["splash"]}')
-
-        # return request
-
-    #     self.logger.info(f'GENERATING start_request response png.')
-    #     imgdata = base64.b64decode(response.data['png'])
-    #     filename = './img/dea_01.png'
-
-    #     with open(filename, 'wb') as f:
-    #         f.write(imgdata)
-
-    #     self.logger.info(f'SAVED start_request response png to {filename}')
-
-
-    #     script = """
-    #         function main(splash, args)
-    #         splash:init_cookies(splash.args.cookies)
-    #         splash:go(splash.args.url)
-
-    #         return {
-    #             html = splash:html(),
-    #             png = splash:png(),
-    #             cookies = splash:get_cookies(),
-    #             }
-    #         end"""
-
-    #     formdata = {'pform:deaNumber':'FE9093028'}
-    #     formRequest = SplashFormRequest(
-    #         url=response.url,
-    #         formdata=formdata,
-    #         callback=self.login_2,
-    #         endpoint='execute',
-    #         session_id=1,
-    #         args={'lua_source': script, 'cookies': response.data['cookies'], 'wait': 10.},
-    #         dont_filter=True,
-    #     )
-    # #
-
-    #     self.logger.info(f'SENDING login_1 REQUEST with \n formdata:{formdata} \n to {formRequest.url} \n and args:{formRequest.meta["splash"]["args"]}')
-
-    #     yield formRequest
-
-
-
-    # def login_2(self, response):
-    #     cookies = response.data['cookies']
-    #     headers = response.data['headers']
-    #     html = response.data['html']
-    #     self.logger.info(f'RECEIVED login_1 response')
-    #     self.logger.info(f'COOKIES in login_1 response: {cookies}')
-    #     self.logger.info(f'HEADERS in login_1 response: {headers}')
-    #     self.logger.info(f'HTML in login_1 response: {html}')
-    # #     self.logger.info(f'RECEIVED login_1 response')
-    # #     self.logger.info(f'COOKIES in login_1 response: {response.data["cookies"]}')
-
-    #     self.logger.info(f'GENERATING login_1 response png.')
-    #     imgdata = base64.b64decode(response.data['png'])
-    #     filename = './img/dea_02.png'
-    #     with open(filename, 'wb') as f:
-    #         f.write(imgdata)
-
-    #     self.logger.info(f'SAVED login_2 response png to {filename}')
-        
-    #     open_in_browser(response)\n to {formRequest.url}
-
-    #     formdata = {
-    #         'csa_lastName': 'Estler', 
-    #         'csa_ssn': '628225151', 
-    #         'csa_zip': '78749', 
-    #         'csa_expMonth': '08', 
-    #         'csa_expYear': '2022',
-    #         'csa_loginTarget': 'validationLogin',
-    #         'submit': 'Login',
-    #     }
-    #     formRequest = FormRequest(url='https://apps.deadiversion.usdoj.gov/webforms2/spring/loginProcess',
-    #                                     formdata=formdata,
-    #                                     callback=self.login_3,
-    #                                     meta={'cookiejar':response.meta['cookiejar']},
-    #                                     dont_filter=True)
-    #     self.logger.info(f'SECOND LOGIN REQUEST with {formdata} \n to {formRequest.url}')
-    #     yield formRequest
-        
 
