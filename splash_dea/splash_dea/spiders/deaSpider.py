20259528cb4419c9488454b701cd494dc8876400
Osvaldo Rosales <osvaldo.rosales@medtrainer.com>
2021-12-15 19:06:19 -0800

    LSFB-37286 - [Update] to dea spider, now we can pass the 2nd login page and fill out the Date of Birth box

--- splash_dea/splash_dea/spiders/deaSpider.py
+++ splash_dea/splash_dea/spiders/deaSpider.py
@@ -27,7 +27,7 @@ class DEASpider(scrapy.Spider):
                 -- Focus input field and fill it with dea number
                 focus('#pform\\\\:deaNumber')
                 splash:send_text('FE9093028')   -- <========== DEA NUMBER
-                assert(splash:wait(0.5))
+                assert(splash:wait(0))
                 pngTable['0'] = splash:png()
 
                 -- Select NEXT button and click it with
@@ -37,24 +37,37 @@ class DEASpider(scrapy.Spider):
 
                 -- Focus input field and fill it with dea number
                 focus('#csa_lastName')
-                splash:send_text('Estler')   -- <========== DEA NUMBER
+                splash:send_text('Estler')   -- <========== LAST NAME
                 assert(splash:wait(1))
                 focus('#csa_ssn')
-                splash:send_text('628225151')   -- <========== DEA NUMBER
+                splash:send_text('628225151')   -- <========== SSN
                 assert(splash:wait(1))
                 focus('#csa_zip')
-                splash:send_text('78749')   -- <========== DEA NUMBER
+                splash:send_text('78749')   -- <========== ZIP CODE
                 assert(splash:wait(1))
                 focus('#csa_expMonth')
-                splash:send_text('08')   -- <========== DEA NUMBER
+                splash:send_text('08')   -- <========== EXP MONTH
                 assert(splash:wait(1))
                 focus('#csa_expYear')
-                splash:send_text('2022')   -- <========== DEA NUMBER
+                splash:send_text('2022')   -- <========== EXP YEAR
                 assert(splash:wait(1))
-                
-
-                assert(splash:wait(5))
                 pngTable['2'] = splash:png()
+
+                -- Select LOGIN button and click it
+                assert(splash:select('input[type=submit]'))
+                splash:select('input[type=submit]'):mouse_click()
+                assert(splash:wait(10))
+                pngTable['3'] = splash:png()
+
+
+                -- Focus input field and fill it with dea number
+                assert(splash:select('input[type=text]'))
+                focus('input[type=text]')
+                splash:send_text('04/27/1988')   -- <========== DATE OF BIRTH
+                assert(splash:wait(1))
+                pngTable['4'] = splash:png()
+
+                
 
 
                 local entries = splash:history()
